8e39c7a7877420d7a94e6417e0c3676e49efe059
Jesse Wolgamott <jesse@comalproductions.com>
2013-09-10 10:58:26 -0500

    Fix: #all is deprecated

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -3,7 +3,7 @@ class TalksController < ApplicationController
   include ActionController::StrongParameters
 
   def index
-    @talks = Talk.order("created_at ASC").all
+    @talks = Talk.order("created_at ASC")
     render json: @talks
   end
 
